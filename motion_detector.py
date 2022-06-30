3ffbec3cf56b7170961d298c53636d273e6e059c
Arjun R Warrier <notwarrier@gmail.com>
2022-06-30 11:04:11 +0530

    capturing difference frames(delta)  and nomal frame(gray frame

--- motion_detector.py
+++ motion_detector.py
@@ -1,24 +1,39 @@
 import  cv2
 
-video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+video = cv2.VideoCapture(0, cv2.CAP_DSHOW)                          #capture video
+
+first_frame = None
 
 while True:
-    check, frame = video.read()
+    check, frame = video.read()                                     #reading video to frame in loop
 
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)            #converting to gray for detection easiness
 
-    cv2.imshow("capture", gray_frame)
+    gray_frame = cv2.GaussianBlur(gray_frame, (21,21),0)            #making the image blur for reducing noise
 
-    key = cv2.waitKey(1)
+    if first_frame is None:
+        first_frame=gray_frame                                       #storing first frame 
+        continue
 
-    print(gray_frame)
 
-    if key==ord('q'):
+    delta_frame = cv2.absdiff(first_frame, gray_frame)               #comparing first frame with remaining frames
+    
+    cv2.imshow("gray frame", gray_frame)                             #showing gray frames
+
+    cv2.imshow("delta frame", delta_frame)                            #showing the difference of frames
+
+
+
+    key = cv2.waitKey(1) 
+
+    print(gray_frame)                                                 #print numpy array
+
+    if key==ord('q'):                                                 #if q is pressed break the loop
         break
 
 
 
 
-video.release()
+video.release()  
 
 cv2.destroyAllWindows
