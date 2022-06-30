6a81ecbe59f818eb1fc88ddc65e6df536c61781b
Arjun R Warrier <notwarrier@gmail.com>
2022-06-30 18:36:37 +0530

    added pandas library to create dataframe and added exporting the result as a csv file

--- motion_detector.py
+++ motion_detector.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-import  cv2
-import time
+import  cv2,time,pandas
 
 first_frame = None                                                  #assigning None to first_frame
 
@@ -8,8 +7,10 @@ status_list = [None,None]                                           #added None 
 
 times = []                                                          #for adding the times to a list
 
+df = pandas.DataFrame(columns=["Start","End"])
+
 video = cv2.VideoCapture(0, cv2.CAP_DSHOW)                          #capture video
-time.sleep(1)
+#time.sleep(1)
                                                  
 
 while True:
@@ -27,7 +28,7 @@ while True:
 
 
     delta_frame = cv2.absdiff(first_frame, gray_frame)               #comparing first frame with remaining frames
-    thresh_frame = cv2.threshold(delta_frame, 100,255,cv2.THRESH_BINARY)[1]#adding a threshold frame change values given accordingly
+    thresh_frame = cv2.threshold(delta_frame, 80,255,cv2.THRESH_BINARY)[1]#adding a threshold frame change values given accordingly
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)           #smoothening the threshold frame
 
     (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  #finding cotours by passing copy of thresh frame
@@ -35,7 +36,7 @@ while True:
 
     
     for contour in cnts:
-        if cv2.contourArea(contour) < 10000:                              #checking if area of contour is less than 1000, true=loop again, false=execute next lines
+        if cv2.contourArea(contour) < 1000:                              #checking if area of contour is less than 1000, true=loop again, false=execute next lines
             continue
         status =1                                                        #changing status
         (x,y,w,h) = cv2.boundingRect(contour)                            #drawing rectangle on the moving object
@@ -47,13 +48,6 @@ while True:
         times.append(datetime.now())
     if status_list[-1]==0 and status_list[-2]==1:
         times.append(datetime.now())
-
-
-
-
-
-
-
 
 
 
@@ -77,9 +71,16 @@ while True:
         break
 
     
-print(status_list)
+print(status_list)                                                   #for reference
 
-print(times)
+print(times)                                                         #for reference
+
+for i in range(0,len(times),2):                                     #from 0 to len(times) with step 2 for skipping the motion end time
+    df=df.append({"Start":times[i],"End":times[i+1]},ignore_index=True) #adding values to dataframe
+
+df.to_csv("Times.csv")                                                #saving it as a csv file
+
+
 
 
 video.release()  
