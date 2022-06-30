106224565bd07113b5b59cae5b3961e6ce8e24e0
Arjun R Warrier <notwarrier@gmail.com>
2022-06-30 12:27:35 +0530

    added a threshold frame

--- motion_detector.py
+++ motion_detector.py
@@ -1,8 +1,11 @@
 import  cv2
-
-video = cv2.VideoCapture(0, cv2.CAP_DSHOW)                          #capture video
+import time
 
 first_frame = None                                                  #assigning None to first_frame
+
+video = cv2.VideoCapture(0, cv2.CAP_DSHOW)                          #capture video
+time.sleep(1)
+                                                 
 
 while True:
     check, frame = video.read()                                     #reading video to frame in loop
@@ -18,10 +21,15 @@ while True:
 
     delta_frame = cv2.absdiff(first_frame, gray_frame)               #comparing first frame with remaining frames
     
+    thresh_frame = cv2.threshold(delta_frame, 100,255,cv2.THRESH_BINARY)[1]#adding a threshold frame
+
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)           #smoothening the threshold frame
+
     cv2.imshow("gray frame", gray_frame)                             #showing gray frames
 
     cv2.imshow("delta frame", delta_frame)                            #showing the difference of frames
 
+    cv2.imshow("thresh frame",thresh_frame)
 
 
     key = cv2.waitKey(1) 
@@ -36,4 +44,4 @@ while True:
 
 video.release()  
 
-cv2.destroyAllWindows
+cv2.destroyAllWindows()
